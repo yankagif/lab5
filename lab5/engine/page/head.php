2cc8b45d6cbcf261bc803a28edf1ccc43aa8c1a1
yankagif <>
2020-11-17 12:24:52 +0500

    Title&Head

---
+++ lab5/engine/page/head.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+<link rel="shortcut icon" type="image/png" href="<? echo URL; ?>/favicon.png">
---
+++ lab5/engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Харченко Яна ПИ-319 Лабораторная работа №5
+        </title>
+	</head>
+</html>
